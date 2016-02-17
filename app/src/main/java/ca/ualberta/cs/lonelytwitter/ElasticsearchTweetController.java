755119c24989234bb584f132ba247c4032b9ae7d
Kent Rasmussen <krasmuss@ualberta.ca>
2016-02-17 12:14:56 -0700

    Add an empty class and some more TODOs

---
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/ElasticsearchTweetController.java
@@ -0,0 +1,10 @@
+package ca.ualberta.cs.lonelytwitter;
+
+/**
+ * Created by esports on 2/17/16.
+ */
+public class ElasticsearchTweetController {
+    //TODO: A function that gets tweets
+
+    //TODO: A function that adds a tweet
+}
